4509527529b5110cc784b396bab52c7158081ee6
harithrawi <86366226+harithrawi@users.noreply.github.com>
2021-06-29 23:28:54 +0800

    Add files via upload

---
+++ index.html.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HRS </title>
+ <!-- style sheet -->
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">"
+ <script type="text/javascript" src="https://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=h6wo3dHP4mEkhaSgg5Kqk_EFtsTgY3ZVB711NALmp7C_9ECJ1d5xE-b9Ceri8x_XMFNhzLuGo3ljpFvg44ZcGAQC1vpfOR2QH3gvYhjWIjO4KXmVt0ed1vK4YD9EYPEMjIfoFGmzdLQ6TRerAh50aZa7_VFmGkQK-LJnqyo6uov3cSClGMOQwMqAo-HUysyNCxZp_OojKGTgsIO4ARD7pOeNiXJPuz4MAVTUmrOw2AKTlhXpMmLeZSLpDwAX1IoZghD0KpuNnGN3YEcAMzqjig2wKHgTo-tiLEOKhc0Tm5wx4175p-IDgYB6quUgbDHT2dI6ETHJaaMh4ofDgCrz7S2l2KFwwHqU67oWQb5FeiwW9FZoAt81bhPAwpYHRMmgbsKxAOnBG6riNqC262jW3iZWbwn3Y_fW5ZYQWUxQmhQ87nZrppLSpNzXYkIDrT4NbWYUz3C9u1KQ83A5YQu1mw" charset="UTF-8"></script><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="style.css">
+<script src="https://kit.fontawesome.com/5a53d05429.js" crossorigin="anonymous"></script>
+ <!-- Google fony -->
+
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">HRS</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Shop</a>
+              </li>
+                <a class="nav-link" href="#">About Us</a>
+            </li>
+                <a class="nav-link" href="#">Contact Us</a>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Category
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">Champion</a></li>
+                  <li><a class="dropdown-item" href="#">Nike</a></li>
+                  <li><a class="dropdown-item" href="#">Puma</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Summer Edition</a></li>
+                </ul>
+            </li>
+        </div>
+      </nav>  
+      <!-- navbar atas close -->
+
+      <!-- seksyen 1 start -->
+      <section class="seksyen1">
+          <div class="container-fluid">
+           <div class="banner">
+            <div class="banner-content">
+                <h1 class="heading-syqn"> HRS <span class="tulisanPutih"></span></h1>
+                <p class="tulisanPutih"> EVERYTHING CAN BE, JUST TRUST </p>
+                <a href="ShopNow.php" class="btn btn-light"><Light> order now </a>
+            </div>
+        <div>
+   </div>
+   </section>
+   <!--  seksyen 1 close -->
+
+       <!-- seksyen 2 start -->
+       <section class="seksyen2">
+      <div class="container-fluid">
+        <h5>free delivery</h5>
+        <br>
+        <div class="row">
+          <div class="feature-box col-md-4">
+            <i class="icon fas fa-male fa-9x"></i>
+           <h4>Champion</h4>
+           <p> promo code Jac79</p>
+          </div>
+          <div class="feature-box col-md-4">
+            <i class= "icon fas fa-child fa-9x" ></i>
+           <h4>Puma</h4>
+           <p> promo code Bieb3R0</p> 
+          </div>
+          <div class="feature-box col-md-4">
+            <i class="icon fas fa-female fa-9x"></i>
+           <h4>Nike</h4>
+           <p>promo code Sc00t6</p> 
+          </div>        
+        </div>
+      </div>
+</section>
+<!-- seksyen 2 close -->
+
+<!-- seksyen 3 start -->
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img1.png" class="d-block w-100" alt="img1">
+    </div>
+    <div class="carousel-item">
+      <img src="img2.png" class="d-block w-100" alt="img2">
+    </div>
+    <div class="carousel-item">
+      <img src="img3.png" class="d-block w-100" alt="img3">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#corouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div> 
+<!-- section 3 close -->
+
+<!-- section 4 start -->
+
+<section class="seksyen4">
+  <h2> Summer Edition Sale</h2>
+  <br>
+  <div class="row">
+    <div class="col-md-4">
+      <div class="card" style="width: 18rem;">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Set A</li>
+          <li class="list-group-item">
+            <p>one set champion</p>
+            <p>Promo Code Jac79 </p>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card" style="width: 18rem;">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Set B</li>
+          <li class="list-group-item">
+            <p>one suit puma</p>
+            <p>Promo Code Bieb3R0 </p>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card" style="width: 18rem;">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Set C</li>
+          <li class="list-group-item">
+            <p> one suit nike </p>
+            <p>Promo Code Sc00t6</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- section 4 close -->
+
+<!-- seksyen 5 start -->
+<section class="seksyen5" id="contact us">
+  <div class="container-fluid">
+    <h2>Contact Information</h2>
+            <br>
+  <form action="" class="row g-3">
+    <div class="mb-3">
+      <label for="exampleFormControlInput1" class="form-label">Email Address</label>
+      <input type="text" class="form-control" id="emailpengirim" placeholder="name@example.com">
+    </div>
+    <div class="mb-3">
+      <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+    <div class="col-12">
+      <a class="btn btn-danger" href="mailto:syqn3@gmail.com.com">Sent</a>
+    </div>
+  </form>
+  </div>
+</section>
+<!-- seksyen 5 close -->
+
+
+<!-- footer -->
+<section class="footer">
+  <a href=""><i class="fab fa-facebook-square"></i></a>
+  <a href=""></a>
+  <a href=""><i class="fab fa-instagram-square"></i></a>
+  <a href=""></a>
+  <i class=""fab fa-instagram></i>
+  <a href=""><i class="fabfa-youtube-square"></i></a>
+  <a href=""><i class="fab fa-youtube-square"></i></a>
+  <a href=""></a>
+  <h6>@Copyright HRS </h6> 
+</section>
+</body>
+</html>
